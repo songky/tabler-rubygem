dcc10ab9d663216c30fe417db55534f2955072a0
kwin <songkuoyin@gmail.com>
2018-12-25 17:48:02 +0800

    selectize

--- assets/javascripts/tabler.js
+++ assets/javascripts/tabler.js
@@ -3,4 +3,5 @@
 // = require tabler/vendors/bootstrap.bundle.min
 // = require tabler/vendors/circle-progress.min
 // = require tabler/vendors/jquery.sparkline.min
+// = require tabler/vendors/selectize.min
 // = require tabler/core
